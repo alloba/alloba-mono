e906ebbecda40cc4ac9d3fe42a7126bd4d561125
Alex Bates <alexlbates@gmail.com>
2023-07-11 19:11:39 -0400

    initial import of kaleidoscope site

---
+++ kaleidoscope/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule} from '@angular/common/http';
+import { ImageDisplayComponent } from './image-display/image-display.component'
+import {FormsModule} from "@angular/forms";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImageDisplayComponent
+  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatCardModule,
+        MatButtonModule
+    ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
