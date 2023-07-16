9a4aed5962c025416d35d55279343629b425844f
Alex Bates <alexlbates@gmail.com>
2023-07-16 15:15:27 -0400

    use ci env

--- infrastructure/build.gradle.kts
+++ infrastructure/build.gradle.kts
@@ -1,11 +1,10 @@
-import org.ysb33r.gradle.terraform.tasks.TerraformValidate
-
 plugins {
     alias(build.plugins.terraform) //terraform
 }
 
 terraform {
     version = "1.5.3"
+    useAwsEnvironment()
 }
 
 terraformSourceSets {
