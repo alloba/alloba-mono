18c87e6f30694d307101ac8169597c84c595b29c
Alex Bates <alexlbates@gmail.com>
2023-10-05 19:31:12 -0400

    move everything to a top level folder instead of nesting in a src folder

--- tf/main.tf
+++ tf/main.tf
@@ -23,12 +23,12 @@ provider "aws" {
 }
 
 module "central-dns" {
-  source = "central-dns"
+  source = "./central-dns"
   route_53_zone_name = "alexlbates.com"
 }
 
 module "personal-site" {
-  source = "cloudfront-s3-site"
+  source = "./cloudfront-s3-site"
 
   acm_certificate_domain = "alexlbates.com"
   cloudfront_distribution_description = "Main landing site for my online presence."
@@ -38,7 +38,7 @@ module "personal-site" {
 }
 
 module "kaleidoscope-site" {
-  source = "cloudfront-s3-site"
+  source = "./cloudfront-s3-site"
   acm_certificate_domain = "alexlbates.com"
   cloudfront_distribution_description = "Kaleidoscope Site"
   route_53_record_names = ["kaleidoscope.alexlbates.com"]
